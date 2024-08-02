a179d06a16b59d1b672387bd1a33cfc25d92d51d
Julia Kim <juliakimseohyeon@gmail.com>
2024-08-01 22:25:48 -0700

    Install CORS

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const notesRoutes = require("./routes/notes-routes");
 require("dotenv").config();
 
 const port = process.env.PORT || 5050;
 app.use(express.json());
+app.use(cors());
 
 app.use("/", notesRoutes);
 
